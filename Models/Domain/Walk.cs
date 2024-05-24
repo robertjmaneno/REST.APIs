cea918cca51a734c9db823aecacf91f9df7316f8
Robert Maneno <robertjmaneno@gmail.com>
2024-05-24 09:46:14 +0200

    Added domain models

---
+++ Models/Domain/Walk.cs
@@ -0,0 +1,22 @@
+﻿namespace REST.APIs.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DifficuiltyId { get; set; }
+
+        public Guid RegionId { get; set; }
+
+
+        //navigation properties
+
+        public Difficuilty Difficuilty { get; set; }
+        public Region Region { get; set; }
+    }
+}
